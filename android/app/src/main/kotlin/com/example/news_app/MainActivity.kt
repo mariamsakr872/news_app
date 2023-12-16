f3963405c6d5c4fc8bba64f3f29118d4a5738edc
Roma <mariamsakr872@gmail.com>
2023-12-16 21:28:20 +0200

    finish news app, add login & register

--- android/app/src/main/kotlin/com/example/news_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/news_app/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.news_app
 
-import io.flutter.embedding.android.FlutterActivity
-
+import io.flutter.embedding.android.FlutterActivity;
 class MainActivity: FlutterActivity() {
 }
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/core/theme/application_theme.dart';
 import 'package:news_app/pages/home/home_view.dart';
 import 'package:news_app/pages/home/widgets/article_details.dart';
+import 'package:news_app/pages/login/login_view.dart';
+import 'package:news_app/pages/register/register_view.dart';
 import 'package:news_app/pages/settings/settings_view.dart';
 import 'package:news_app/pages/splash/splash_view.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
 
 import 'app_provider.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(ChangeNotifierProvider(
       create: (BuildContext context) => AppProvider(), child: const MyApp()));
 }
@@ -32,6 +41,8 @@ class MyApp extends StatelessWidget {
         HomeView.routeName: (context) => const HomeView(),
         SettingView.routeName: (context) => const SettingView(),
         ArticleDetails.routeName: (context) => const ArticleDetails(),
+        LoginView.routeName: (context) =>  const LoginView(),
+        RegisterView.routeName: (context) => const RegisterView(),
       },
     );
   }
